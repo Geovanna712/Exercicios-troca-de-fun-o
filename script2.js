c13e9b1f481faa451d9ccb49d4399b161749ab70
Geovanna <geovannasantosreis15@gmail.com>
2024-09-25 17:29:42 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+let pessoa=1800
+let valor=150
+let lucro 
+let lucroAnterior
+
+lucro=(pessoa*valor) - 1000
+
+ do{
+
+    lucroAnterior=lucro
+
+    pessoa=pessoa+50
+    valor=valor-10
+
+    lucro=(pessoa*valor) - 1000
+
+}while(lucro>lucroAnterior)
+
+console.log(valor)
---
+++ script3.js
@@ -0,0 +1,10 @@
+let i
+
+let num= Number(prompt("Digite um numero da tabuada?"))
+
+for(i=0; i<=10;i++) {
+
+    let multiplicacao=num*i
+
+    console.log(`${num} X ${i} = ${multiplicacao}`)
+}
